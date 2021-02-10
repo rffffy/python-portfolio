10b6b8079dad4d2f1d5e64b0b136ac167698e0c7
rffffy <raafay.alam@gmail.com>
2021-02-10 23:10:59 +0100

    adding files for portfolio website

---
+++ server.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, redirect
+import csv
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def render_html_page(page_name):
+    return render_template(page_name)
+
+
+# def write_to_file(data):
+#     with open('contact_data.csv', mode='a') as contact_data:
+#         email = data["email"]
+#         subject = data["subject"]
+#         message = data["message"]
+#
+#         file = contact_data.write(f'\n{email},{subject},{message}')
+
+
+def write_to_csv(data):
+    with open('contact_data.csv', newline='', mode='a') as contact_data:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+
+        csv_writer = csv.writer(contact_data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/submit_form', methods=['POST'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('thankyou.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'something went wrong'
